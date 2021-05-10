8398cedb9654ad3e79676fc30b16d6e4934f391e
Frank <fcnatra@yahoo.com>
2021-05-10 20:34:34 +0200

    Cálculo de la distancia - y la distancia en X e Y - DONE

--- Coord/Program.cs
+++ Coord/Program.cs
@@ -6,7 +6,7 @@ namespace Coord
     {
         static void Main(string[] args)
         {
-            args = new string[] { "1", "1", "5" };
+            if (args.Length == 0) args = new string[] { "5.581595877671971", "-0.17829882552805323", "5" }; // 622,98kms
 
             ShowHelp();
             new RunCalculation(args);
@@ -24,20 +24,87 @@ namespace Coord
 
     public class RunCalculation
     {
+        private const float EARTH_RADIUS = 6378.0f;//6367.45f; //
+
         public RunCalculation(string[] args)
         {
             GeoCoordinate geoCoordinate = new GeoCoordinate(args[0], args[1]);
             var quadrantSizeInKm = int.Parse(args[2]);
 
+            double distanceKm = GetDistance(new GeoCoordinate(0, 0), geoCoordinate);
+            Console.WriteLine($"Distance between points: {distanceKm}Km");
 
+            GeoDistance geoDistanceInKm = GetGeoDistance(new GeoCoordinate(0,0), geoCoordinate);
+
+            Console.WriteLine($"\nDistance in Lat axel (Y): {geoDistanceInKm.LatAxel}Km");
+            Console.WriteLine($"Distance in Lon axel (X): {geoDistanceInKm.LongAxel}Km");
+        }
+
+        private GeoDistance GetGeoDistance(GeoCoordinate geoCoordinateFrom, GeoCoordinate geoCoordinateTo)
+        {
+            var distanceInLatAxel = GetDistance(geoCoordinateFrom, new GeoCoordinate(geoCoordinateTo.Latitude, geoCoordinateFrom.Longitude));
+            var distanceInLontAxel = GetDistance(geoCoordinateFrom, new GeoCoordinate(geoCoordinateFrom.Latitude, geoCoordinateTo.Longitude));
+
+            var geoDistance = GeoDistance.From(geoCoordinateTo);
+            geoDistance.LatAxel = distanceInLatAxel;
+            geoDistance.LongAxel = distanceInLontAxel;
+
+            return geoDistance;
+        }
+
+        private double GetDistance(GeoCoordinate geoCoordinateFrom, GeoCoordinate geoCoordinateTo)
+        {
+            // https://www.genbeta.com/desarrollo/como-calcular-la-distancia-entre-dos-puntos-geograficos-en-c-formula-de-haversine
+
+            float latDelta = EnRadianes(geoCoordinateTo.Latitude - geoCoordinateFrom.Latitude);
+            float lontDelta = EnRadianes(geoCoordinateTo.Longitude - geoCoordinateFrom.Longitude);
+
+            double a = Math.Pow(Math.Sin(latDelta / 2), 2)
+                + Math.Cos(EnRadianes(geoCoordinateFrom.Latitude))
+                * Math.Cos(EnRadianes(geoCoordinateTo.Latitude))
+                * Math.Pow(Math.Sin(lontDelta / 2), 2);
+
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            double distanceKm = EARTH_RADIUS * c;
+
+            return distanceKm;
+        }
+
+        private float EnRadianes(float valor)
+        {
+            return Convert.ToSingle(Math.PI / 180) * valor;
         }
     }
 
+    public class GeoDistance : GeoCoordinate
+    {
+        public static GeoDistance From(GeoCoordinate geoCoordinate)
+        {
+            return new GeoDistance
+            {
+                Latitude = geoCoordinate.Latitude,
+                Longitude = geoCoordinate.Longitude
+            };
+        }
+
+        public double LatAxel { get; set; }
+        public double LongAxel { get; set; }
+    }
 
     public class GeoCoordinate
     {
         public float Latitude { get; set; }
         public float Longitude { get; set; }
+
+        public GeoCoordinate()
+        {}
+
+        public GeoCoordinate(float latitude, float longitude)
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
 
         public GeoCoordinate(string latitude, string longitude)
         {
